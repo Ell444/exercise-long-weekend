25f14dd30a14eb03fc245ea4b6a2765bdb038119
Laura Sirigu <laurasirigu43@gmail.com>
2023-12-07 16:03:42 +0100

    feat || esercizio 1 e 2

---
+++ src/components/CustomList.jsx
@@ -0,0 +1,14 @@
+
+const CustomList = ({ type, list }) => {
+
+
+    const lista = list.map((el, ix) =>
+
+        <li key={ix}>{el}</li>
+
+    )
+
+    return type === 'ul' ? <ul>{lista}</ul> : <ol>{lista}</ol>
+}
+
+export default CustomList;
